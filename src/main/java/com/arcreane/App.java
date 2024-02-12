ef31e935ca931ef85c389ffd410bfb709a415680
Dominique YOLIN <d.yolin@isartdigital.com>
2024-02-12 15:41:55 +0100

    Introduction to spring context

--- src/main/java/com/arcreane/App.java
+++ src/main/java/com/arcreane/App.java
@@ -3,6 +3,8 @@ package com.arcreane;
 import com.arcreane.controller.IMovieController;
 import com.arcreane.repository.IMovieRepository;
 import com.arcreane.service.IMovieService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.util.Scanner;
 
@@ -11,36 +13,39 @@ import java.util.Scanner;
  */
 public class App {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        String packageName = "com.arcreane.";
-        IMovieController controller = null;
-        IMovieService service = null;
-        IMovieRepository repository = null;
-
-        System.out.println("Which controller name do you want to use");
-        String controllerName = sc.nextLine();
-        controllerName = packageName+ "controller." + controllerName + "MovieController";
-
-        System.out.println("Which service do you want to use");
-        String serviceName = sc.nextLine();
-        serviceName = packageName + "service." + serviceName + "MovieService";
-
-        System.out.println("Which Repository do you want to use");
-        String repositoryName = sc.nextLine();
-        repositoryName = packageName+ "repository." + repositoryName + "MovieRepository";
-
-        try {
-            controller = (IMovieController) Class.forName(controllerName).getDeclaredConstructor().newInstance();
-            service = (IMovieService) Class.forName(serviceName).getDeclaredConstructor().newInstance();
-            repository = (IMovieRepository) Class.forName(repositoryName).getDeclaredConstructor().newInstance();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        controller.setService(service);
-        service.setRepository(repository);
-
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        IMovieController controller = context.getBean(IMovieController.class);
         controller.AddMovie();
+//        Scanner sc = new Scanner(System.in);
+//
+//        String packageName = "com.arcreane.";
+//        IMovieController controller = null;
+//        IMovieService service = null;
+//        IMovieRepository repository = null;
+//
+//        System.out.println("Which controller name do you want to use");
+//        String controllerName = sc.nextLine();
+//        controllerName = packageName+ "controller." + controllerName + "MovieController";
+//
+//        System.out.println("Which service do you want to use");
+//        String serviceName = sc.nextLine();
+//        serviceName = packageName + "service." + serviceName + "MovieService";
+//
+//        System.out.println("Which Repository do you want to use");
+//        String repositoryName = sc.nextLine();
+//        repositoryName = packageName+ "repository." + repositoryName + "MovieRepository";
+//
+//        try {
+//            controller = (IMovieController) Class.forName(controllerName).getDeclaredConstructor().newInstance();
+//            service = (IMovieService) Class.forName(serviceName).getDeclaredConstructor().newInstance();
+//            repository = (IMovieRepository) Class.forName(repositoryName).getDeclaredConstructor().newInstance();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//
+//        controller.setService(service);
+//        service.setRepository(repository);
+//
+//        controller.AddMovie();
     }
 }
